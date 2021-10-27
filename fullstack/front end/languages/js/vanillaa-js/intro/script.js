b1933a93e94193c9ef53f29bd5ceb5044271f5b3
mrgowda223 <90748006+mrgowda223@users.noreply.github.com>
2021-10-27 21:04:41 +0530

    ddd

--- fullstack/front end/languages/js/vanillaa-js/intro/script.js
+++ fullstack/front end/languages/js/vanillaa-js/intro/script.js
@@ -2,4 +2,4 @@ function calu() {
     alert("hello im in page js:out put on alert1")
     document.write('<h1>hello out put on document 2</h1>')
     console.log("im oot put on console 3rd way")
-}
+}     
